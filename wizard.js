2739b291edf433a29b305e4a92572665e9799e03
Twebb2590 <Twebb2590@gmail.com>
2026-06-13 11:23:15 -0400

    Update wizard.js

--- wizard.js
+++ wizard.js
@@ -204,7 +204,7 @@ async function startWizard() {
   );
 
   setQuickReplies([
-    { label: 'I’m sick or injured', value: 'I'm sick or injured.' },
+    { label: 'I’m sick or injured', value: 'sick' },
     { label: 'Pregnancy or birth', value: 'pregnancy' },
     { label: 'Caring for a family member', value: 'family_care' },
     { label: 'Bereavement or loss', value: 'bereavement' },
