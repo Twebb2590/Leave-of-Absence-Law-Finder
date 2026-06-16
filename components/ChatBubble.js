7ab4cb019f6850da6dd58ab9443fd5b092f6b6a4
Twebb2590 <Twebb2590@gmail.com>
2026-06-15 22:07:07 -0400

    Update ChatBubble.js

--- components/ChatBubble.js
+++ components/ChatBubble.js
@@ -1,4 +1,10 @@
 // components/ChatBubble.js
+
+function linkify(text) {
+  const urlRegex = /(https?:\/\/[^\s]+)/g;
+  return text.replace(urlRegex, url => `<a href="${url}" target="_blank">${url}</a>`);
+}
+
 export function createChatBubble({ text, from = 'assistant' }) {
   const wrapper = document.createElement('div');
   wrapper.className = `chat-row ${from}`;
